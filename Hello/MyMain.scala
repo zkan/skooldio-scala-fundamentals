0482c161b8fc237fae65f182acc46514528b440b
zkan <zkan.cs@gmail.com>
2022-08-03 07:42:06 +0700

    Get XML and parse it

--- Hello/MyMain.scala
+++ Hello/MyMain.scala
@@ -1,3 +1,6 @@
+import scalaj.http.{Http, HttpResponse}
+import scala.xml.{XML, Elem, NodeSeq}
+
 // object MyMain {
 //   def main(params: Array[String]) = {
 //     println("Hello World")
@@ -6,4 +9,20 @@
 
 object MyMain extends App {
     println("Hello World")
+
+    val response: HttpResponse[String] = Http(
+        "http://feeds.bbci.co.uk/news/rss.xml"
+    ).asString
+    // println(response)
+
+    val xml: Elem = XML.loadString(response.body)
+    // println(xml)
+
+    // val titles: NodeSeq = xml.\\("item").\("title")
+    val titles: NodeSeq = xml \\  "item" \ "title"
+    // println(titles)
+
+    val titleTexts: Seq[String] = for { title <- titles } yield title.text
+    // titleTexts.foreach(titleText => println(titleText))
+    titleTexts.foreach(println)
 }
